ab13deaf0752c0a68bb5d18a9320abe2aca3c4a2
ma-h-m <mahaoming8585485@163.com>
2025-03-03 13:04:30 -0500

    upload

--- skeleton lab2/lab2.c
+++ skeleton lab2/lab2.c
@@ -160,7 +160,7 @@ void print_display_buffer() {
         // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col, is_cursor);
         fbputchar('=', CHAT_ROWS, col, 0);
         // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col);
-        previous_display_buffer[row][col] = display_buffer[row][col];
+        // previous_display_buffer[row][col] = display_buffer[row][col];
       }
     }
   }
