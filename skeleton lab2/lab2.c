bb95b4e98f7d629e881cc9880d9a6f61c730f70b
ma-h-m <mahaoming8585485@163.com>
2025-03-03 13:01:06 -0500

    upload

--- skeleton lab2/lab2.c
+++ skeleton lab2/lab2.c
@@ -156,6 +156,7 @@ void print_display_buffer() {
         // bool is_cursor = (row * CHAT_COLS + col) == cursor_pos;
         bool is_cursor = 0;
         printf("row: %d, col: %d, is_cursor: %d\n", row, col, is_cursor);
+        printf("Char: %c\n", display_buffer[row][col]);
         fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col, is_cursor);
         // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col);
         previous_display_buffer[row][col] = display_buffer[row][col];
