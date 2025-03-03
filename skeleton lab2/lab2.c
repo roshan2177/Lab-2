f7e326cf27898475efc862936a57789f2f0b5d01
ma-h-m <mahaoming8585485@163.com>
2025-03-03 13:04:54 -0500

    upload

--- skeleton lab2/lab2.c
+++ skeleton lab2/lab2.c
@@ -158,9 +158,9 @@ void print_display_buffer() {
         printf("row: %d, col: %d, is_cursor: %d\n", row, col, is_cursor);
         printf("Char: %d\n", display_buffer[row][col]);
         // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col, is_cursor);
-        fbputchar('=', CHAT_ROWS, col, 0);
+        // fbputchar('=', CHAT_ROWS, col, 0);
         // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col);
-        // previous_display_buffer[row][col] = display_buffer[row][col];
+        previous_display_buffer[row][col] = display_buffer[row][col];
       }
     }
   }
