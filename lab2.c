41475b7c3b65010b6d8974eb682d51cfbc735d96
ma-h-m <mahaoming8585485@163.com>
2025-03-06 10:26:25 -0500

    Remove commented code from lab2.c

--- lab2.c
+++ lab2.c
@@ -1,3 +1,9 @@
+/*
+ *
+ * CSEE 4840 Lab 2 for 2019
+ *
+ * Name/UNI: Haoming Ma (hm3070), Roshan Prakash (rp3187)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,21 +33,21 @@ void send_message_to_server(char *message);
 void fbclear() {
   for (int row = 0; row < 24; row++) {
       for (int col = 0; col < 64; col++) {
-          fbputchar(' ', row, col, 0);  // 用空格覆盖整个屏幕
+          fbputchar(' ', row, col, 0); 
       }
   }
 }
 void fbclear_chat() {
   for (int row = 0; row < CHAT_ROWS; row++) {
       for (int col = 0; col < CHAT_COLS; col++) {
-          fbputchar(' ', row, col, 0);  // 用空格覆盖整个屏幕
+          fbputchar(' ', row, col, 0);  
       }
   }
 }
 void fbclear_input(){
   for (int row = CHAT_ROWS + 1; row < 24; row++) {
       for (int col = 0; col < 64; col++) {
-          fbputchar(' ', row, col, 0);  // 用空格覆盖整个屏幕
+          fbputchar(' ', row, col, 0);  
       }
   }
 }
@@ -55,7 +61,6 @@ void add_chat_message(const char *msg){
         int len = (msg_len - start < CHAT_COLS) ? (msg_len - start) : CHAT_COLS;
         int line  = (chat_start + chat_count) % CHAT_ROWS;
         strncpy(char_buffer[line], msg + start, len);
-        // printf("char_buffer[%d]: %s\n", line, char_buffer[line]);
         for(int i = len; i < CHAT_COLS; i++) {
             char_buffer[line][i] = '\0';
         }
@@ -65,7 +70,6 @@ void add_chat_message(const char *msg){
             chat_start = (chat_start + 1) % CHAT_ROWS;
         }
         start += len;
-        // chat_buffer[line][len] = '\0';
 
     }
 }
@@ -73,7 +77,6 @@ void redraw_chat(){
     fbclear_chat();
     for(int i = 0; i < chat_count; i++) {
         int line = (chat_start + i) % CHAT_ROWS;
-        // printf("char_buffer[%d]: %s\n", line, char_buffer[line]);
         fbputs(char_buffer[line], i, 0);
     }
 }
@@ -95,27 +98,19 @@ char display_buffer[INPUT_ROWS][CHAT_COLS];
 char previous_display_buffer[INPUT_ROWS][CHAT_COLS];
 int cursor_pos = 0;
 int display_start = 0;
-// int input_length = 0;
 int start_idx = 0;
 void update_display_buffer(){
-  // printf("input_buffer: %s\n", input_buffer);
   for (int i = 0; i < INPUT_ROWS; i++)
   {
     memset(display_buffer[i], ' ', CHAT_COLS);
-    // display_buffer[i][CHAT_COLS - 1] = '\0';
   }
-  // printf("input_buffer: %s\n", input_buffer);
   start_idx = (cursor_pos < CHAT_COLS * INPUT_ROWS) ? 0 : ((cursor_pos ) / CHAT_COLS - 1 ) * CHAT_COLS;
   int len_to_display = INPUT_ROWS * CHAT_COLS > INPUT_BUFFER_SIZE - start_idx ? INPUT_BUFFER_SIZE - start_idx : INPUT_ROWS * CHAT_COLS;
-  // printf("len_to_display: %d\n", len_to_display);
-  // printf("start_idx: %d\n", start_idx);
-  // printf("input_buffer: %s\n", input_buffer);
   if(len_to_display > 0){
     int line_idx = 0;
     for (int i = 0; i < len_to_display; i++)
     {
       display_buffer[line_idx][i % CHAT_COLS] = input_buffer[start_idx + i];
-      // if (line_idx == 0 && i < 20) printf("display_buffer[%d][%d]: %c\n", line_idx, i % CHAT_COLS, display_buffer[line_idx][i % CHAT_COLS]);
       if ((i + 1) % CHAT_COLS == 0)
       {
         line_idx++;
@@ -123,114 +118,29 @@ void update_display_buffer(){
     }
     
   }
-  // printf("display_buffer: %s\n", display_buffer);
 }
-
-// void print_display_buffer(){
-//   for(int row = 0; row < INPUT_ROWS; row++) {
-//     for(int col = 0; col < CHAT_COLS; col++) {
-//       if (display_buffer[row][col] != previous_display_buffer[row][col]) {
-//         fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col);
-//         previous_display_buffer[row][col] = display_buffer[row][col];
-//       }
-//     }
-//   }
-// }
 #include <pthread.h>
 
 pthread_mutex_t display_mutex = PTHREAD_MUTEX_INITIALIZER;
 #include<stdbool.h>
 char tmp[CHAT_COLS + 1];
 void print_display_buffer() {
-  pthread_mutex_lock(&display_mutex); // 加锁，确保线程安全
-  // fbopen();
-  // cursor_pos++;
+  pthread_mutex_lock(&display_mutex); 
   int cursor_row = cursor_pos / CHAT_COLS + CHAT_ROWS + 1 - start_idx / CHAT_COLS;
   int cursor_col = cursor_pos % CHAT_COLS + 1;
-  // cursor_pos--;
   fbclear_input();
-  // printf("========================\n");
-  // printf("cursor_row: %d\n", cursor_row);
-  // printf("cursor_col: %d\n", cursor_col);
   for(int row = 0; row < INPUT_ROWS; row++) {
-    // printf("row: %d\n", row);
-    // printf("cursor_row: %d\n", cursor_row);
-    // printf("cursor_col: %d\n", cursor_col);
-    // printf("display_buffer[row]: %s\n", display_buffer[row]);
     memcpy(tmp, display_buffer[row], CHAT_COLS);
     tmp[CHAT_COLS] = '\0';
-    // printf("tmp: %s\n", tmp);
-    // printf("display_buffer[row]: %s\n", display_buffer[row]);
-    // printf("cursor_pos_char: %d\n", display_buffer[cursor_row][cursor_col]);
-    // if (cursor_row - 21 != row ){
-    //   fbputs(tmp, row + CHAT_ROWS + 1, 0);
-    //   continue;
-    // }
     fbputs_with_cursor(tmp, row + CHAT_ROWS + 1, 0, cursor_row, cursor_col);
-    // fbputs_with_cursor(display_buffer[row], row + CHAT_ROWS + 1, 0, cursor_row, cursor_col);
-    // fbputs_with_cursor(display_buffer[row], row + CHAT_ROWS + 1, 0, -1, -1);
-    // for(int col = 0; col < CHAT_COLS; col++) {
-      
-      // if (display_buffer[row][col] != previous_display_buffer[row][col]) {
-      //   // bool is_cursor = (row * CHAT_COLS + col) == cursor_pos;
-      //   bool is_cursor = 0;
-      //   // printf("row: %d, col: %d, is_cursor: %d\n", row, col, is_cursor);
-      //   // printf("Char: %d\n", display_buffer[row][col]);
-      //   fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col, is_cursor);
-      //   // fbputchar('=', CHAT_ROWS, col, 0);
-      //   // fbputchar(display_buffer[row][col], row + CHAT_ROWS + 1, col);
-      //   previous_display_buffer[row][col] = display_buffer[row][col];
-      // }
-    // }
+
   }
 
-  pthread_mutex_unlock(&display_mutex); // 解锁
+  pthread_mutex_unlock(&display_mutex); 
 }
 
 void handle_input(char c){
-    // static int esc_sequence = 0;
-    // if (esc_sequence == 0 && c == 27) {
-    //     esc_sequence = 1;
-    //     return;
-    // }
-    // if (esc_sequence == 1 && c == 91) {
-    //     esc_sequence = 2;
-    //     return;
-    // }
-    // if (esc_sequence == 2){
-    //     switch (c)
-    //     {
-    //     case 65:
-    //         // up arrow
-    //         printf("up arrow pressed\n");
-    //         break;
-    //     case 66:
-    //         // down arrow
-    //         printf("down arrow pressed\n");
-    //         break;
-    //     case 67:
-    //         // right arrow
-    //         if (cursor_pos < INPUT_BUFFER_SIZE - 1 && input_buffer[cursor_pos] != '\0') {
-    //           cursor_pos++;
-    //           printf("right arrow pressed\n");
-    //           update_display_buffer();
-    //           print_display_buffer();
 
-    //         }
-    //         break;
-    //     case 68:
-    //         // left arrow
-    //         if (cursor_pos > 0) {
-    //             printf("left arrow pressed\n");
-    //           cursor_pos--;
-    //           update_display_buffer();
-    //           print_display_buffer();
-    //         }
-    //         break;
-    //     default:
-    //         break;
-    //     }
-    // }
   if (c == '\n') {
     // send message
     printf("send message: %s\n", input_buffer);
@@ -284,11 +194,10 @@ void handle_input(char c){
           }
           input_buffer[cursor_pos] = c;
           
-          // printf("input_buffer: %s\n", input_buffer);/
+
           cursor_pos++;
           update_display_buffer();
-          // printf("display_buffer: %s\n", display_buffer[0]);
-          // printf("display_buffer: %s\n", display_buffer[1]);
+
           print_display_buffer();
         }
       }
@@ -299,14 +208,11 @@ void handle_input(char c){
 }
 
 
-// char input_buffer[MAX_INPUT_LENGTH];
-// int buffer_pos = 0;
 int use_terminal_input = 0;  // Fallback if USB keyboard isn't found
 
 /* USB to ASCII functuon*/
 char usb_to_ascii(uint8_t keycode, uint8_t modifiers) {
-    // printf("keycode: %0x\n", keycode);
-    // printf("modifiers: %0x\n", modifiers);
+
     static char ascii_map[256] = {0};
     ascii_map[0x04] = 'a'; ascii_map[0x05] = 'b'; ascii_map[0x06] = 'c';
     ascii_map[0x07] = 'd'; ascii_map[0x08] = 'e'; ascii_map[0x09] = 'f';
@@ -487,9 +393,7 @@ int main() {
         printf("Chat client started. Use the USB keyboard to type messages.\n");
         uint8_t prev_keys[MAX_KEYS] = {0};
         while (1) {
-            // libusb_interrupt_transfer(keyboard, endpoint_address,
-            //                           (unsigned char *) &packet, sizeof(packet),
-            //                           &transferred, 0);
+
             int r = libusb_interrupt_transfer(keyboard, endpoint_address,
                 (unsigned char *) &packet, sizeof(packet),
                 &transferred, 0);
@@ -517,27 +421,10 @@ int main() {
                             handle_input(usb_to_ascii(key, packet.modifiers));
                         }
                     }
-                    // if (packet.keycode[i] != 0) {
-                    //     handle_input(usb_to_ascii(packet.keycode[i], packet.modifiers));
-                    // }
+
                 }
                 memcpy(prev_keys, packet.keycode, MAX_KEYS);
-                // char key = usb_to_ascii(packet.keycode[0], packet.modifiers);
-                // if (key) {
-                //     if (key == '\b' && buffer_pos > 0) {  // Backspace handling
-                //         buffer_pos--;
-                //         input_buffer[buffer_pos] = '\0';
-                //         printf("\b \b"); // Erases the last charecter in the input buffer
-                //     } else if (key == '\n') {  // Uses enter to sent message
-                //         input_buffer[buffer_pos] = '\0';
-                //         send_message_to_server(input_buffer);
-                //         buffer_pos = 0;  // Used to rest the buffer
-                //     } else if (buffer_pos < MAX_INPUT_LENGTH - 1) {  
-                //         input_buffer[buffer_pos++] = key;
-                //         input_buffer[buffer_pos] = '\0';
-                //         printf("%c", key);  // Used to pring the typed charecter
-                //     }
-                // }
+
 
                 /* Use to escape*/
                 if (packet.keycode[0] == 0x29) {
